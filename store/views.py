18a9420d56d55263e4b188c1a461095391b0cb3b
saurav <shresthsaurav8@gmail.com>
2024-06-18 12:59:32 +0545

    feat: add dynamic listing of product with img (store.html)

--- store/views.py
+++ store/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def index(request):
-    context = {}
-    return render(request, 'store/store.html')
+
+    products = Product.objects.all()
+
+    context = {'products':products}
+    return render(request, 'store/store.html', context)
 
 def cart(request):
     context = {}
